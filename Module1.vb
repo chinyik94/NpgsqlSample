6f1fcfdd248aa55568826ffb585ccddb68ce9b81
zach.hong <chinyik.hong@tetsuyuhealthcare.com>
2024-12-10 11:59:56 +0800

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Using context As New C4WX1Entities
+            Dim errorLog As New ErrorLog
+
+            errorLog.ErrorDetails = "Test Error"
+            errorLog.DateCreated = Now()
+            context.ErrorLog.Add(errorLog)
+
+            context.SaveChanges()
+        End Using
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("NpgsqlSample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("NpgsqlSample")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a1f8b0e4-b387-42ac-8505-8b2a7c9c911c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
